892317dc4e079ac3ca392785f5386cf862e16c6f
ChSPN <charlene.scomparin@gmail.com>
2024-02-22 08:24:18 +0100

    Write a program that prints _putchar, followed by a new line

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int _putchar(char c);
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char result[] = "_putchar";
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(result[i]);
+	}
+
+	_putchar('\n');
+	return (0);
+}
