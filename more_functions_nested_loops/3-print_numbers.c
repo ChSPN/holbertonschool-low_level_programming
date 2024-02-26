bf989c89cfc824324f8f93c1f07b4ba9eda74432
ChSPN <charlene.scomparin@gmail.com>
2024-02-26 17:50:35 +0100

    Write a function that prints the numbers, from 0 to 9, followed by a new line

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * Print numbers - print the numbers from 0 to 9 to stdout
+ * @c: character to test
+ *
+ * Return: On success 1.
+ * On error, 0 is returned, and errno is set appropriately.
+ */
+void print_numbers(void)
+{
+
+	char c = '0';
+
+		while (c <= '9')
+		{
+			putchar (c);
+		}
+	putchar ('\n');
+
+	return(0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,4 +2,4 @@ int _isupper(int c);
 
 int _isdigit(int c);
 
-
+void print_numbers(void);
