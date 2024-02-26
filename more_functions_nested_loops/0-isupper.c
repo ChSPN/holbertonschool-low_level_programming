fd27792053c7269fc2313b7a3991f3dda8b9659e
ChSPN <charlene.scomparin@gmail.com>
2024-02-26 10:12:12 +0100

    Write a function that checks for uppercase character

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c);
+
+{
+
+	int c = alphabet
+		if (isupper(c))
+		cout << c << "1 \n";
+		{
+			return (1);
+		}
+	else
+	{
+		return (0);
+	}
+}
+
