c8f80547c441634903118d92fc8f1d5d4b348d58
ChSPN <charlene.scomparin@gmail.com>
2024-02-26 11:02:38 +0100

    Write a function that checks for a digit (0 through 9)

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _isdigit - check the character digit to stdout
+ * @c: The character to test
+ *
+ * Return: On success 1.
+ * On error, 0 is returned, and errno is set appropriately.
+ */
+int _isdigit(int c);
+
+{
+
+	char digit[] = "0123456789";
+	int i;
+
+	for (i = 0; i < 10; i++)
+	
+	{
+		if (digit[i] == c)
+		{
+			return (1);
+		}
+	}
+
+	return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
