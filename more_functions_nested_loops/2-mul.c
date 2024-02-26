4070b08e733be9c555f4c93dd90f99ecb5a36b8e
ChSPN <charlene.scomparin@gmail.com>
2024-02-26 18:41:53 +0100

    Write a function that multiplies two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiply a by b to stdout
+ * @a : int a
+ * @b : int b
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -9,4 +9,6 @@ int _isdigit(int c);
 
 void print_numbers(void);
 
+int mul(int a, int b);
+
 #endif
