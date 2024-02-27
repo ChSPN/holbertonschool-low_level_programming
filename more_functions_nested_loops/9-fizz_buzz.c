2b8ef69256029da62a83319106a6da85e49bd9f9
ChSPN <charlene.scomparin@gmail.com>
2024-02-27 09:18:34 +0100

    The “Fizz-Buzz test”

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,4 @@
-#include "_putchar.c"
+#include "stdio.h"
 
 /**
  * main - print the numbers from 1 to 100
@@ -12,22 +12,22 @@ int main(void)
 	{
 		if (i > 1)
 		{
-			_putchar(' ');
+			putchar(' ');
 		}
 
 		if ((i % 3) == 0)
 		{
-			_putchar('F');
-			_putchar('i');
-			_putchar('z');
-			_putchar('z');
+			putchar('F');
+			putchar('i');
+			putchar('z');
+			putchar('z');
 		}
 		else if ((i % 5) == 0)
 		{
-			_putchar('B');
-			_putchar('u');
-			_putchar('z');
-			_putchar('z');
+			putchar('B');
+			putchar('u');
+			putchar('z');
+			putchar('z');
 		}
 		else
 		{
@@ -38,13 +38,13 @@ int main(void)
 			else
 			{
 				r = i % 10;
-				_putchar((i / 10) + '0');
+				putchar((i / 10) + '0');
 			}
 
-			_putchar(r + '0');
+			putchar(r + '0');
 		}
 	}
 
-	_putchar('\n');
+	putchar('\n');
 	return (0);
 }
