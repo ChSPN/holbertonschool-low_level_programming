dda2796121b96b9886f17966a8b537bfb8c09898
ChSPN <charlene.scomparin@gmail.com>
2024-02-27 09:24:30 +0100

    The “Fizz-Buzz test”

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -6,45 +6,40 @@
  */
 int main(void)
 {
-	int i, r;
+	int i, r, mod3, mod5;
 
 	for (i = 1; i <= 100; i++)
 	{
 		if (i > 1)
-		{
 			putchar(' ');
-		}
-
-		if ((i % 3) == 0)
+		mod3 = i % 3;
+		if (mod3 == 0)
 		{
 			putchar('F');
 			putchar('i');
 			putchar('z');
 			putchar('z');
 		}
-		else if ((i % 5) == 0)
+		mod5 = i % 5;
+		if (mod5 == 0)
 		{
 			putchar('B');
 			putchar('u');
 			putchar('z');
 			putchar('z');
 		}
-		else
+		if (mod3 != 0 && mod5 != 0)
 		{
 			if (i < 10)
-			{
 				r = i;
-			}
 			else
 			{
 				r = i % 10;
 				putchar((i / 10) + '0');
 			}
-
 			putchar(r + '0');
 		}
 	}
-
 	putchar('\n');
 	return (0);
 }
