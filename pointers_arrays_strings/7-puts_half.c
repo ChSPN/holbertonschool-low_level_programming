efefa1fa02feb323cf1a495c8bc756c4c42f0d9d
ChSPN <charlene.scomparin@gmail.com>
2024-02-29 08:12:13 +0100

    Write a function that prints half of a string, followed by a new line

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - function that prints a reverse string.
+ * Description: function that prints a reverse string.
+ * @str: chaine de caracteres "My School".
+ * Return: blabla
+ */
+void puts_half(char *str)
+{
+	int length = 0, half, i;
+
+	while (*str != '\0')
+	{
+		length++;
+		str++;
+	}
+
+	half = length / 2;
+
+	for (i = half; i < length; i++)
+	{
+		str--;
+	}
+
+	for (i = half; i < length; i++)
+	{
+		_putchar(*str);
+		str++;
+	}
+
+	_putchar('\n');
+}
