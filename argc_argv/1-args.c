6d05ebfd7776329ee3a5449781c19956fc765746
ChSPN <charlene.scomparin@gmail.com>
2024-03-08 10:17:08 +0100

    Write a program that prints the number of arguments passed into it

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - function that swaps the values of two integers
+ * Description: function that swaps the values of two integers
+ * @argc: c
+ * @argv: v
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	char *prog = argv[0];
+
+	prog++;
+	printf("%d\n", argc - 1);
+	return (0);
+}
