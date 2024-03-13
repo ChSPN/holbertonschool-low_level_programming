32a08d101fde7119b0a3499ff2fb8f79b30c33bf
ChSPN <charlene.scomparin@gmail.com>
2024-03-13 21:54:00 +0100

    Write a function that concatenates two strings

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -3,6 +3,22 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - string len
+ * @s: string
+ *
+ * Return: int len
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+/**
  * string_nconcat - check the code
  * check the code
  * @s1: string one
@@ -12,53 +28,30 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i, size1 = 0, size2 = 0;
 	char *s = NULL;
-	unsigned int size1 = 0;
-	unsigned int size2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
-
-	while (*s1 != '\0')
-	{
-		size1++;
-		s1++;
-	}
-
-	s1--;
-	while (*s2 != '\0')
-	{
-		size2++;
-		s2++;
-	}
-
-	s2--;
-	while (n < size2)
-	{
-		size2--;
-		s2--;
-	}
-
+	size1 = _strlen(s1);
+	size2 = _strlen(s2);
+	if (n < size2)
+		size2 = n;
 	s = malloc(sizeof(char) * (size1 + size2 + 1));
 	if (s == NULL)
 		return (NULL);
-
-	for (i = size1; i > 0; i--)
+	for (i = 0; i < size1; i++)
 	{
-		s[i - 1] = *s1;
-		s1--;
+		s[i] = *s1;
+		s1++;
 	}
-
-	for (i = size1 + size2 - 1; i >= size1; i--)
+	for (i = size1; i < size1 + size2; i++)
 	{
 		s[i] = *s2;
-		s2--;
+		s2++;
 	}
-
 	s[size1 + size2] = '\0';
 	return (s);
 }
