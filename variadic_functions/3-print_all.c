0fa61a963987688ec19ce9603bc4ff9b778d601e
ChSPN <charlene.scomparin@gmail.com>
2024-03-22 19:40:14 +0100

    Write a function that prints anything

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -6,48 +6,52 @@
  * print_c - function
  * Description: print_c
  * @args: args
+ * Return: char
 */
-void print_c(va_list args)
+char print_c(va_list args)
 {
 	int v = va_arg(args, int);
 
-	printf("%c", v);
+	return (v);
 }
 
 /**
  * print_i - function
  * Description: print_i
  * @args: args
+ * Return: int
 */
-void print_i(va_list args)
+int print_i(va_list args)
 {
 	int v = va_arg(args, int);
 
-	printf("%d", v);
+	return (v);
 }
 
 /**
  * print_f - function
  * Description: print_f
  * @args: args
+ * Return: float
 */
-void print_f(va_list args)
+double print_f(va_list args)
 {
 	double v = va_arg(args, double);
 
-	printf("%f", v);
+	return (v);
 }
 
 /**
  * print_s - function
  * Description: print_s
  * @args: args
+ * Return: char
 */
-void print_s(va_list args)
+char *print_s(va_list args)
 {
 	char *v = va_arg(args, char*);
 
-	printf("%s", v == NULL ? "(nil)" : v);
+	return (v == NULL ? "(nil)" : v);
 }
 
 /**
@@ -57,38 +61,36 @@ void print_s(va_list args)
 */
 void print_all(const char * const format, ...)
 {
-	int i = 0;
 	int j = 0;
-	va_list args;
-	ft formats[] = {
-		{"c", print_c},
-		{"i", print_i},
-		{"f", print_f},
-		{"s", print_s}
-	};
+	char *space;
+	va_list ap;
 
-	va_start(args, format);
-	while (format[i] != '\0')
+	va_start(ap, format);
+
+	while (format && format[j])
 	{
-		j = 0;
-		while (j < 4 && *formats[j].format != format[i])
+		space = "";
+		if (format[j + 1])
+			space = ", ";
+
+		switch (format[j])
 		{
-			j++;
+			case 'c':
+				printf("%c%s", print_c(ap), space);
+				break;
+			case 'i':
+				printf("%d%s", print_i(ap), space);
+				break;
+			case 'f':
+				printf("%f%s", print_f(ap), space);
+				break;
+			case 's':
+				printf("%s%s", print_s(ap), space);
+				break;
 		}
 
-		if (j < 4)
-		{
-			if (i > 0)
-			{
-				printf(", ");
-			}
-
-			formats[j].print(args);
-		}
-
-		i++;
+		j++;
 	}
 
 	printf("\n");
-	va_end(args);
 }
