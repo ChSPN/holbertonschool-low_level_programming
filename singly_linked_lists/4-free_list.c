a6908da466085b40f9e743541e970b043c906dff
ChSPN <charlene.scomparin@gmail.com>
2024-04-03 15:40:14 +0200

    Write a function that frees a list_t list.

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,6 +1,6 @@
-#include "lists.h"
 #include <stdlib.h>
 #include <string.h>
+#include "lists.h"
 
 /**
  * add_node_end - Adds a new node at the end of a list_t list.
@@ -9,38 +9,15 @@
  *
  * Return: The address of the new element, or NULL if it failed.
  */
-list_t *add_node_end(list_t **head, const char *str)
+void free_list(list_t *head)
 {
-    list_t *new_node;
-    list_t *last = *head;
+    list_t *temp;
 
-    if (str == NULL)
-        return NULL;
-
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return NULL;
-
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
+    while (head != NULL)
     {
-        free(new_node);
-        return NULL;
+        temp = head;
+        head = head->next;
+        free(temp->str);
+        free(temp);
     }
-
-    new_node->len = strlen(str);
-    new_node->next = NULL;
-
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return new_node;
-    }
-
-    while (last->next != NULL)
-        last = last->next;
-
-    last->next = new_node;
-
-    return new_node;
 }
