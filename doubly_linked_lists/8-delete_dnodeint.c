32b80a187b9a0abe582efa8ac32852131ff7927b
ChSPN <charlene.scomparin@gmail.com>
2024-04-04 22:08:59 +0200

    Fix Betty

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -10,34 +10,34 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current, *temp;
-    unsigned int i;
+	dlistint_t *current, *temp;
+	unsigned int i;
 
-    if (head == NULL || *head == NULL)
-        return (-1);
+	if (head == NULL || *head == NULL)
+		return (-1);
 
-    current = *head;
+	current = *head;
 
-    if (index == 0)
-    {
-        *head = current->next;
-        if (current->next != NULL)
-            current->next->prev = NULL;
-        free(current);
-        return (1);
-    }
+	if (index == 0)
+	{
+		*head = current->next;
+		if (current->next != NULL)
+			current->next->prev = NULL;
+		free(current);
+		return (1);
+	}
 
-    for (i = 0; current != NULL && i < index - 1; i++)
-        current = current->next;
+	for (i = 0; current != NULL && i < index - 1; i++)
+		current = current->next;
 
-    if (current == NULL || current->next == NULL)
-        return (-1);
+	if (current == NULL || current->next == NULL)
+		return (-1);
 
-    temp = current->next->next;
-    free(current->next);
-    current->next = temp;
-    if (temp != NULL)
-        temp->prev = current;
+	temp = current->next->next;
+	free(current->next);
+	current->next = temp;
+	if (temp != NULL)
+		temp->prev = current;
 
-    return (1);
+	return (1);
 }
