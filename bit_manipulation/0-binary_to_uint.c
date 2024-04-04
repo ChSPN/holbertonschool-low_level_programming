722588cd8537b15a9234a4f1fe7d4e07f43f343e
ChSPN <charlene.scomparin@gmail.com>
2024-04-04 22:14:55 +0200

    Fix 0-binary_to_uint.c Betty

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -9,26 +9,26 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int result = 0;
+	unsigned int result = 0;
 
-    /* Check if the input string is NULL */
-    if (b == NULL)
-        return 0;
+	/* Check if the input string is NULL */
+	if (b == NULL)
+		return (0);
 
-    /* Iterate through the input string */
-    while (*b)
-    {
-        /* If a character is not '0' or '1', return 0 */
-        if (*b != '0' && *b != '1')
-            return 0;
+	/* Iterate through the input string */
+	while (*b)
+	{
+		/* If a character is not '0' or '1', return 0 */
+		if (*b != '0' && *b != '1')
+			return (0);
 
-        /* Shift the result left by 1 bit and add the current binary digit */
-        result = (result << 1) + (*b - '0');
+		/* Shift the result left by 1 bit and add the current binary digit */
+		result = (result << 1) + (*b - '0');
 
-        /* Move to the next character in the string */
-        b++;
-    }
+		/* Move to the next character in the string */
+		b++;
+	}
 
-    /* Return the converted result */
-    return result;
+	/* Return the converted result */
+	return (result);
 }
