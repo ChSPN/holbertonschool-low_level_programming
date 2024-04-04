7d6e0ba6e119386709a5f673983d5ad92c8187db
ChSPN <charlene.scomparin@gmail.com>
2024-04-04 21:35:39 +0200

    Fix 4-free_list betty

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -3,21 +3,20 @@
 #include "lists.h"
 
 /**
- * add_node_end - Adds a new node at the end of a list_t list.
+ * free_list - Adds a new node at the end of a list_t list.
  * @head: A pointer to a pointer to the head of the list.
- * @str: The string to be duplicated and stored in the new node.
  *
  * Return: The address of the new element, or NULL if it failed.
  */
 void free_list(list_t *head)
 {
-    list_t *temp;
+	list_t *temp;
 
-    while (head != NULL)
-    {
-        temp = head;
-        head = head->next;
-        free(temp->str);
-        free(temp);
-    }
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
 }
