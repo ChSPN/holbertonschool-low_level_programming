aa35c3fc99262c0a8d6405ec437262cb84aa069e
ChSPN <charlene.scomparin@gmail.com>
2024-04-05 15:47:03 +0200

    Write a program that copies the content of a file to another file.

---
+++ file_io/3-cp.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * main - Copies the content of a file to another file.
+ * @argc: Number of arguments passed to the program.
+ * @argv: Array of pointers to the arguments.
+ *
+ * Return: 0 on success, otherwise 97, 98, 99, or 100.
+ */
+int main(int argc, char *argv[])
+{
+    int fd_from, fd_to, rd, wr;
+    char buffer[BUFFER_SIZE];
+
+    /* Check number of arguments */
+    if (argc != 3)
+    {
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
+
+    /* Open source file */
+    fd_from = open(argv[1], O_RDONLY);
+    if (fd_from == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
+
+    /* Open or create destination file */
+    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+    if (fd_to == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+        close(fd_from);
+        exit(99);
+    }
+
+    /* Copy content */
+    while ((rd = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+    {
+        wr = write(fd_to, buffer, rd);
+        if (wr == -1 || wr != rd)
+        {
+            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+            close(fd_from);
+            close(fd_to);
+            exit(99);
+        }
+    }
+
+    if (rd == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        close(fd_from);
+        close(fd_to);
+        exit(98);
+    }
+
+    /* Close file descriptors */
+    if (close(fd_from) == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+        exit(100);
+    }
+    if (close(fd_to) == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+        exit(100);
+    }
+
+    return (0);
+}
